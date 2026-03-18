e1f106a521d24571c557826dff3ed3a5063a7169
evelincabrera139-bin <evelincabrera139@gmail.comm>
2026-03-18 17:09:31 -0600

    first commit

---
+++ src/practica1.java
@@ -0,0 +1,11 @@
+package src;
+
+public class practica1 {
+    int numero1 = 5;
+    int numero2 = 10;
+
+    public static void main(String[] args) {
+        practica1 obj = new practica1();
+        System.out.println("Hola Mundo " + (obj.numero1 + obj.numero2));
+    }
+}
